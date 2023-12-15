11329e90abc80124e35e3cdf4bfc54580844df7b
Lev <lev.fedorichev@aisa.ru>
2023-12-15 08:21:39 +0300

    Изменение логике для поддержки не параллельных стен

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,11 +4,8 @@ using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
-using Microsoft.SqlServer.Server;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 namespace RevitPlugin
 {
@@ -36,25 +33,6 @@ namespace RevitPlugin
                 TaskDialog.Show("Ошибка", "Выберите хотя бы 4 стены для модификации перекрытия.");
                 return Result.Failed;
             }
-        }
-
-        private Floor GetSelectedSlab(Document doc)
-        {
-            // Получаем выбранный элемент (перекрытие)
-            UIDocument uidoc = new UIDocument(doc);
-            ICollection<ElementId> selectedIds = uidoc.Selection.GetElementIds();
-
-            foreach (ElementId id in selectedIds)
-            {
-                Element elem = doc.GetElement(id);
-
-                if (elem is Floor slab)
-                {
-                    return slab;
-                }
-            }
-
-            return null;
         }
 
         private List<Wall> GetSelectedWalls(UIDocument uiDoc, Document doc)
@@ -112,21 +90,22 @@ namespace RevitPlugin
 
         private List<XYZ> GetPoints(List<Curve> wallCurves, double offset, double minOffset, Level level)
         {
-            List<XYZ> wallCorners = GetWallCorners(wallCurves);
-            XYZ topLeftOuter = new XYZ(wallCorners[0].X - offset, wallCorners[0].Y + offset, 0);
-            XYZ bottomLeftOuter = new XYZ(wallCorners[1].X - offset, wallCorners[1].Y - offset, 0);
-            XYZ bottomRightOuter = new XYZ(wallCorners[2].X + offset, wallCorners[2].Y - offset, 0);
-            XYZ topRightOuter = new XYZ(wallCorners[3].X + offset, wallCorners[3].Y + offset, 0);
+            Square square = GetSquare(wallCurves);
 
-            XYZ topLeftInner = new XYZ(wallCorners[0].X - minOffset, wallCorners[0].Y + minOffset, 0);
-            XYZ bottomLeftInner = new XYZ(wallCorners[1].X - minOffset, wallCorners[1].Y - minOffset, 0);
-            XYZ bottomRightInner = new XYZ(wallCorners[2].X + minOffset, wallCorners[2].Y - minOffset, 0);
-            XYZ topRightInner = new XYZ(wallCorners[3].X + minOffset, wallCorners[3].Y + minOffset, 0);
+            XYZ topLeftOuter = new XYZ(square.left - offset, square.top + offset, 0);
+            XYZ bottomLeftOuter = new XYZ(square.left - offset, square.bottom - offset, 0);
+            XYZ bottomRightOuter = new XYZ(square.right + offset, square.bottom - offset, 0);
+            XYZ topRightOuter = new XYZ(square.right + offset, square.top + offset, 0);
 
-            XYZ topLeft = new XYZ(wallCorners[0].X, wallCorners[0].Y, level.Elevation);
-            XYZ topRight = new XYZ(wallCorners[1].X, wallCorners[1].Y, level.Elevation);
-            XYZ bottomRight = new XYZ(wallCorners[2].X, wallCorners[2].Y, level.Elevation);
-            XYZ bottomLeft = new XYZ(wallCorners[3].X, wallCorners[3].Y, level.Elevation);
+            XYZ topLeftInner = new XYZ(square.left - minOffset, square.top + minOffset, 0);
+            XYZ bottomLeftInner = new XYZ(square.left - minOffset, square.bottom - minOffset, 0);
+            XYZ bottomRightInner = new XYZ(square.right + minOffset, square.bottom - minOffset, 0);
+            XYZ topRightInner = new XYZ(square.right + minOffset, square.top + minOffset, 0);
+
+            XYZ topLeft = new XYZ(square.left, square.top, level.Elevation - 0.2);
+            XYZ bottomLeft = new XYZ(square.left, square.bottom, level.Elevation - 0.2);
+            XYZ bottomRight = new XYZ(square.right, square.bottom, level.Elevation - 0.2);
+            XYZ topRight = new XYZ(square.right, square.top, level.Elevation - 0.2);
 
             List<XYZ> outerPolygon = new List<XYZ>() { 
                 topLeftOuter, 
@@ -138,9 +117,9 @@ namespace RevitPlugin
                 bottomRightInner,
                 topRightInner, 
                 topLeft, 
-                topRight, 
+                bottomLeft, 
                 bottomRight, 
-                bottomLeft 
+                topRight
             };
             return outerPolygon;
         }
@@ -149,54 +128,78 @@ namespace RevitPlugin
         {
             List<PolymeshFacet> facets = new List<PolymeshFacet>();
 
-            PolymeshFacet facet1 = new PolymeshFacet(0, 1, 4);
-            facets.Add(facet1);
-            PolymeshFacet facet2 = new PolymeshFacet(1, 4, 5);
-            facets.Add(facet2);
-            PolymeshFacet facet3 = new PolymeshFacet(1, 5, 2);
-            facets.Add(facet3);
-            PolymeshFacet facet4 = new PolymeshFacet(2, 5, 6);
-            facets.Add(facet4);
-            PolymeshFacet facet5 = new PolymeshFacet(2, 6, 3);
-            facets.Add(facet5);
-            PolymeshFacet facet6 = new PolymeshFacet(3, 6, 7);
-            facets.Add(facet6);
-            PolymeshFacet facet7 = new PolymeshFacet(3, 7 , 0);
-            facets.Add(facet7);
-            PolymeshFacet facet8 = new PolymeshFacet(4, 7, 0);
-            facets.Add(facet8);
+            PolymeshFacet outerFacet1 = new PolymeshFacet(0, 1, 4);
+            facets.Add(outerFacet1);
+            PolymeshFacet outerFacet2 = new PolymeshFacet(1, 4, 5);
+            facets.Add(outerFacet2);
+            PolymeshFacet outerFacet3 = new PolymeshFacet(1, 5, 2);
+            facets.Add(outerFacet3);
+            PolymeshFacet outerFacet4 = new PolymeshFacet(2, 5, 6);
+            facets.Add(outerFacet4);
+            PolymeshFacet outerFacet5 = new PolymeshFacet(2, 6, 3);
+            facets.Add(outerFacet5);
+            PolymeshFacet outerFacet6 = new PolymeshFacet(3, 6, 7);
+            facets.Add(outerFacet6);
+            PolymeshFacet outerFacet7 = new PolymeshFacet(3, 7 , 0);
+            facets.Add(outerFacet7);
+            PolymeshFacet outerFacet8 = new PolymeshFacet(4, 7, 0);
+            facets.Add(outerFacet8);
 
 
-            PolymeshFacet facet11 = new PolymeshFacet(4, 5, 8);
-            facets.Add(facet11);
-            PolymeshFacet facet21 = new PolymeshFacet(5, 8, 9);
-            facets.Add(facet21);
-            PolymeshFacet facet31 = new PolymeshFacet(5, 9, 6);
-            facets.Add(facet31);
-            PolymeshFacet facet41 = new PolymeshFacet(6, 9, 10);
-            facets.Add(facet41);
-            PolymeshFacet facet51 = new PolymeshFacet(6, 10, 7);
-            facets.Add(facet51);
-            PolymeshFacet facet61 = new PolymeshFacet(7, 10, 11);
-            facets.Add(facet61);
-            PolymeshFacet facet71 = new PolymeshFacet(7, 11, 4);
-            facets.Add(facet71);
-            PolymeshFacet facet81 = new PolymeshFacet(8, 11, 4);
-            facets.Add(facet81);
+            PolymeshFacet middleFacet1 = new PolymeshFacet(4, 5, 8);
+            facets.Add(middleFacet1);
+            PolymeshFacet middleFacet2 = new PolymeshFacet(5, 8, 9);
+            facets.Add(middleFacet2);
+            PolymeshFacet middleFacet3 = new PolymeshFacet(5, 9, 6);
+            facets.Add(middleFacet3);
+            PolymeshFacet middleFacet4 = new PolymeshFacet(6, 9, 10);
+            facets.Add(middleFacet4);
+            PolymeshFacet middleFacet5 = new PolymeshFacet(6, 10, 7);
+            facets.Add(middleFacet5);
+            PolymeshFacet middleFacet6 = new PolymeshFacet(7, 10, 11);
+            facets.Add(middleFacet6);
+            PolymeshFacet middleFacet7 = new PolymeshFacet(7, 11, 4);
+            facets.Add(middleFacet7);
+            PolymeshFacet middleFacet8 = new PolymeshFacet(8, 11, 4);
+            facets.Add(middleFacet8);
+
+            PolymeshFacet innerFacet1 = new PolymeshFacet(8, 9, 11);
+            facets.Add(innerFacet1);
+            PolymeshFacet innerFacet2 = new PolymeshFacet(10, 9, 11);
+            facets.Add(innerFacet2);
 
             return facets;
         }
 
-        private List<XYZ> GetWallCorners(List<Curve> wallCurves)
+        private Square GetSquare(List<Curve> wallCurves)
         {
-            List<XYZ> wallCorners = new List<XYZ>();
+            Square square = new Square(0,0,0,0);
 
             foreach (Curve curve in wallCurves)
             {
-                wallCorners.Add(curve.GetEndPoint(0));
+                square.left = Math.Min(curve.GetEndPoint(0).X, square.left);
+                square.right = Math.Max(curve.GetEndPoint(0).X, square.right);
+                square.bottom = Math.Min(curve.GetEndPoint(0).Y, square.bottom);
+                square.top = Math.Max(curve.GetEndPoint(0).Y, square.top);
             }
 
-            return wallCorners;
+            return square;
+        }
+
+        class Square
+        {
+            public double left;
+            public double top;
+            public double bottom;
+            public double right;
+
+            public Square(double left, double top, double bottom, double right)
+            {
+                this.left = left;
+                this.top = top;
+                this.bottom = bottom;
+                this.right = right;
+            }
         }
 
     }
